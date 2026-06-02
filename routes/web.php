9c11cec3cda356d9650bd17bd9e6fdb88c04767b
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2026-06-01 22:07:28 -0300

    feat: criação inicial de uma lista de email

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\EmailListController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    Auth::loginUsingId(1);
+    return to_route('dashboard');
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('email-list', [EmailListController::class, 'index'])->name('email-list.index');
+    Route::get('email-list/create', [EmailListController::class, 'create'])->name('email-list.create');
+    Route::post('email-list/store', [EmailListController::class, 'store'])->name('email-list.store');
 });
 
 require __DIR__.'/auth.php';

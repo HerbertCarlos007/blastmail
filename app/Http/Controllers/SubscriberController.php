e8f94b19d943c80d199f74691d97dec270178bef
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2026-06-04 03:10:56 -0300

    feat: adicionar um subscriber em uma lista

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -7,6 +7,7 @@ use App\Models\EmailList;
 use App\Models\Subscriber;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class SubscriberController extends Controller
 {
@@ -28,6 +29,24 @@ class SubscriberController extends Controller
                 ->paginate(5),
 
         ]);
+    }
+
+    public function create(EmailList $emailList)
+    {
+        return view('subscriber.create', compact('emailList'));
+    }
+
+    public function store(EmailList $emailList)
+    {
+        $data = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', Rule::unique('subscribers')->where('email_list_id', $emailList->id)],
+        ]);
+
+        $emailList->subscribers()->create($data);
+
+        return to_route('subscribers.index', $emailList)
+            ->with('message', __('Subscriber created.'));
     }
 
     public function destroy(mixed $emailList, Subscriber $subscriber)
